6bb254fb1ad90ca71a205a2ec43f2299215f7a59
Mehwish Aziz <mehwisha@msn.com>
2019-09-14 03:29:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const drivers = ['Milo', 'Otis', 'Garfield'];
 function destructivelyAppendDriver() {
 drivers.push('Bob');  
 }
+function destructivelyPrependDriver() {
+  
+}
+function destructivelyRemoveLastDriver() {
+  
+}
